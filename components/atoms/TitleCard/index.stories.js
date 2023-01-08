9e666667139951be768e988e61bd7290d26f5c67
Matthew Nascimento <matthewromie@hotmail.com>
2023-01-08 09:43:29 -0300

    Add: Storybook

---
+++ components/atoms/TitleCard/index.stories.js
@@ -0,0 +1,11 @@
+import TitleCard from ".";
+
+export default {
+    title: "Atoms/TitleCard",
+    component: TitleCard,
+    args: {},
+};
+
+export const Avatar = () => <TitleCard name="Avatar" />;
+
+
