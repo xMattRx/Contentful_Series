05af112e40f0b274e4ef279c58e16e9a7d54252c
matthew-nascimento_valtech <matthew.nascimento@valtech.com>
2024-06-25 15:48:16 -0300

    feat: update title

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,13 @@
+import Head from 'next/head'
 import '../styles/globals.scss'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>My Next.js App</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
